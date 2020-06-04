bfb42a9f739dbc0e8ba8eb4a4d2615db2d618c1a
omicron-b <linuxgaming@protonmail.com>
2020-06-04 06:18:46 +0000

    - update degoogle.sh for Android 10 release

--- degoogle.sh
+++ degoogle.sh
@@ -6,4 +6,5 @@ adb shell pm uninstall --user 0 com.google.android.apps.magazines
 adb shell pm uninstall --user 0 com.google.ar.lens
 adb shell pm uninstall --user 0 com.google.android.apps.messaging
 adb shell pm uninstall --user 0 com.google.android.apps.photos
+adb shell pm uninstall --user 0 com.google.android.projection.gearhead
 echo "The (partial) degoogling is now finished. Please, reboot your phone, e.g. 'adb reboot'."
